89377af5d295fadbaae1fffc66ccc3b36c9e746f
abanoubgamal33 <abanoubgamal33@gmail.com>
2022-09-03 14:09:31 +0200

    commit

--- app/src/main/java/com/example/asteroidRadarApp/model/Constants.kt
+++ app/src/main/java/com/example/asteroidRadarApp/model/Constants.kt
@@ -5,3 +5,12 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
 }
+
+/**
+ *
+key > 5Js2fYGmi6RmGYe5778s0mAbQwgUgEz1cb1sqz0h
+link today image > https://api.nasa.gov/planetary/apod?api_key=key
+start = 2015-09-07 ,end = 2015-09-08
+link all with date > https://api.nasa.gov/neo/rest/v1/feed?start_date=start&end_date=end&api_key=key
+
+ **/
